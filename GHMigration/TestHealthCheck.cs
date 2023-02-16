2066efd13b49f71f50fc19514d074e21668e0562
PhilBroderickCrezco <110662030+PhilBroderickCrezco@users.noreply.github.com>
2023-02-16 11:51:33 +0000

    Add basic health check to test app service health checks (#3)

---
+++ GHMigration/TestHealthCheck.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace GHMigration;
+
+public class TestHealthCheck : IHealthCheck
+{
+    private readonly TestHealthCheckOptions _options;
+
+    public TestHealthCheck(TestHealthCheckOptions options)
+    {
+        _options = options;
+    }
+    
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var healthCheckResult = _options.ReportHealthy
+            ? HealthCheckResult.Healthy()
+            : HealthCheckResult.Unhealthy();
+        
+        return Task.FromResult(healthCheckResult);
+    }
+}
+
+public class TestHealthCheckOptions
+{
+    public bool ReportHealthy { get; set; }
+}
